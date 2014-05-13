cd7f6e98f8eaeda2ac96dc32d74c4770e8bd9eb5
christopherdumas <christopherdumas@gmail.com>
2014-05-13 08:34:23 -0700

    I've finished. Let me save.

--- app/routes/tickets/index.js
+++ app/routes/tickets/index.js
@@ -1,5 +1,8 @@
 export default Ember.Route.extend({
   model: function() {
     return this.store.findAll('ticket');
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
   }
 });
